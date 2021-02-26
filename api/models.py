c408dc52b5aed7437038391a913814a26037f2ae
ZaidBaalbaki <62421514+ZaidBaalbaki@users.noreply.github.com>
2021-02-26 22:39:25 +0200

    finished part 5

--- api/models.py
+++ api/models.py
@@ -3,17 +3,17 @@ import string
 import random
 
 
-def generate_uniue_code():
+def generate_unique_code():
     length = 6
     while True:
-        code = ''.join(random.choices(string.ascii_uppercase))
-        if Room.object.filter(code=code).count()==0:
+        code = ''.join(random.choices(string.ascii_uppercase,k= length))
+        if Room.objects.filter(code=code).count() == 0:
             break
-        return code
+    return code
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='')
+    code = models.CharField(max_length=8, default=generate_unique_code,unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False,default=1)
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component, component } from "react";
 import { render } from "react-dom";
+import HomePage from './Homepage'
+
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return <h1>testing react code</h1>
+        return <div>
+            <HomePage/>
+        </div> ;
     }
 }
 const appDiv = document.getElementById("app");
-render(<App/>,appDiv);
+render(<App />,appDiv);
