1e409a49100d5e89d178154a2b0d36bc718e71a6
ZaidBaalbaki <62421514+ZaidBaalbaki@users.noreply.github.com>
2021-02-26 23:54:15 +0200

    part 6

---
+++ frontend/src/components/Room.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Room extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            votesToSkip:2,
+            guestCanPause: false,
+            isHost: false,
+        };
+    }
+    render(){
+        return <div>
+            <p></p>
+        </div>
+    }
+}
